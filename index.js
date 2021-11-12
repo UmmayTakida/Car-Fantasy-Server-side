f69838a37bb9b75e239bce905f49716ddcaffc35
UmmayTakida <utakida08@gmail.com>
2021-11-13 02:43:29 +0600

    products added

--- index.js
+++ index.js
@@ -51,9 +51,11 @@ async function run() {
             const id = req.params.id;
             console.log('getting specific product', id);
             const query = { _id: ObjectId(id) };
-            const product = await ordersCollection.findOne(query);
+            const product = await productsCollection.findOne(query);
             res.json(product);
         })
+
+
         app.get('/allOrders/:id', async (req, res) => {
             const id = req.params.id;
             const query = {
